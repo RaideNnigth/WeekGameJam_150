214ad298eb121f57d629e1f136d0163d7a23e3bb
RaideNnigth <32282947+RaideNnigth@users.noreply.github.com>
2020-05-23 01:04:04 -0300

    First Move and hit system
    please read my code see if everything is ok and after it merge it

---
+++ WeekGameJam_150/Assets/Player/FirePoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePoint : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firepoint;
+    public float shootDelay;
+    private bool canShoot = false;
+    
+    void Update()
+    {
+        if (canShoot == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(bullet, firepoint.position, firepoint.rotation);
+                canShoot = true;
+                StartCoroutine(wait());
+            }
+        }
+    }
+
+    IEnumerator wait() 
+    {
+        yield return new WaitForSeconds(shootDelay);
+        canShoot = false;
+    
+    }
+}
---
+++ WeekGameJam_150/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, 5f);
+    }
+}
