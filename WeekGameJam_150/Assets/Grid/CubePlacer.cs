a814be2507af8abd8bce37eb4ffe55f8d67a4425
RaideNnigth <32282947+RaideNnigth@users.noreply.github.com>
2020-05-24 01:37:18 -0300

    Fixed some bugs again and add more features
    Like timer and limitation

--- WeekGameJam_150/Assets/Grid/CubePlacer.cs
+++ WeekGameJam_150/Assets/Grid/CubePlacer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization.Formatters;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +10,12 @@ public class CubePlacer : MonoBehaviour
 {
     public GameObject[] prefab;
     public Image[] Itens;
+    public Text limitCounter;
+    public int limitOne = 4;
+    public int limitTwo = 4;
+
     int prefabSelected = 0;
+
     Grid grid;
     
 
@@ -26,17 +33,23 @@ public class CubePlacer : MonoBehaviour
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo))
             {
-                
-
                 if (hitInfo.collider.tag != "Traps") 
                 {
+                    if (prefabSelected == 0 && limitOne > 0)
+                    {
+                            limitOne -= 1;
+                            limitCounter.text = limitOne.ToString();
+                            PlaceCubeNear(hitInfo.point);
+                    }
 
-                    PlaceCubeNear(hitInfo.point);
+                    if (prefabSelected == 1 && limitTwo > 0)
+                    {   
+                        limitTwo -= 1;
+                        limitCounter.text = limitTwo.ToString();
+                        PlaceCubeNear(hitInfo.point);
+                    }
 
-                    //Hide the cursor and show a selection of the grid
                     Cursor.visible = false;
-
-                    //To show a square
 
                 }
                 
@@ -49,7 +62,7 @@ public class CubePlacer : MonoBehaviour
             Cursor.visible = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+            if (Input.GetKeyDown(KeyCode.Alpha1)) 
         {
             prefabSelected = 0;
         }
@@ -85,13 +98,27 @@ public class CubePlacer : MonoBehaviour
 
         }
 
+        if (Input.GetKeyDown(KeyCode.X))
+            {
+                if (prefabSelected == 0  && limitOne < 4) 
+                {
+                    Destroy(GameObject.Find("Spikes(Clone)"));
+                    limitOne += 1;
+                }
+
+                if (prefabSelected == 1 && limitTwo < 4)
+                {
+                    Destroy(GameObject.Find("BearTrap(Clone)"));
+                    limitTwo += 1;
+                } 
+            }
         SpriteChooser();
     }
 
     private void PlaceCubeNear(Vector3 point)
     {
         Vector3 finalPos = grid.GetNearestPointOnGrid(point);
-        Instantiate(prefab[prefabSelected], finalPos, Quaternion.identity);
+        Instantiate(prefab[prefabSelected], finalPos, Quaternion.identity);      
     }
 
     private void SpriteChooser() 
@@ -100,12 +127,14 @@ public class CubePlacer : MonoBehaviour
         {
             Itens[1].enabled = false;
             Itens[0].enabled = true;
+            limitCounter.text = limitOne.ToString();
         }
         
         if (prefabSelected == 1)
         {
             Itens[0].enabled = false;
             Itens[1].enabled = true;
+            limitCounter.text = limitTwo.ToString();
         }
     }
 }
