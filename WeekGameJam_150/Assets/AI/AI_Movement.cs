a7206092a047d8dfd59159da5611e0ed27c12cc5
BhavsarJay <bhavsarjay01@gmail.com>
2020-05-24 22:37:49 +0530

    Added Ai Player movement

---
+++ WeekGameJam_150/Assets/AI/AI_Movement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float lookRadius;
+    public float speed;
+    public float stoppingDist;
+    GameObject target;
+
+    [Space]
+    [Header("Shooting")]
+    public GameObject bullet;
+    public Transform FirePoint;
+    public float TimeBtwnShots = 0.5f;
+    private float lastShot;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("enemies");
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+
+        if (distance <= lookRadius)
+        {
+            if (distance >= stoppingDist)
+            {
+                transform.Translate((target.transform.position - transform.position) * speed * Time.deltaTime, Space.World);
+            }
+
+            //Face Target
+            FaceTarget();
+
+            //Shoot
+            float startTime = Time.time;
+            if (Time.time > TimeBtwnShots + lastShot)
+            {
+                Instantiate(bullet, FirePoint.transform.position, transform.rotation);
+                lastShot = Time.time;
+            }
+        }
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = target.transform.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        Gizmos.DrawWireSphere(transform.position, stoppingDist);
+    }
+}
