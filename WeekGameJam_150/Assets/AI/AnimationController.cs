f819911046312a60300775115d8c33e68fefdbba
BhavsarJay <bhavsarjay01@gmail.com>
2020-05-25 03:14:21 +0530

    Added Minions Movement

---
+++ WeekGameJam_150/Assets/AI/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator animator;
+    AI_Movement movement;
+    private void Start()
+    {
+        movement = GetComponent<AI_Movement>();
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(movement.isRunning == true)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
