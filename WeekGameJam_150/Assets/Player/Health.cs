778b9990e94d5f931451ee962e1ba026c67144a7
RaideNnigth <32282947+RaideNnigth@users.noreply.github.com>
2020-05-23 22:03:32 -0300

    Bugs fixed and i add some features

---
+++ WeekGameJam_150/Assets/Player/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int numOfHearths;
+    public Image[] hearts;
+
+    void Update() 
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            
+            if (i < numOfHearths)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+
+    public void GetDamaged() 
+    {
+        numOfHearths--; 
+    }
+}
